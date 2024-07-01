893b633dbf480c1c0aed542f489d58389d0e49dd
vinith <vinithamm007@gmail.com>
2024-07-01 18:00:19 +0530

    rpm

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,8 @@
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+curl -sL https://rpm.nodesource.com/setup_lts.x
+
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
