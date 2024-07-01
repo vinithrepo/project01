19f4feb23264de328b9300b583ecf847f7ed14cc
vinith <vinithamm007@gmail.com>
2024-07-01 17:03:42 +0530

    added catalogue script

---
+++ catalogue.sh
@@ -0,0 +1,28 @@
+
+
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+dnf install mongodb-org-shell -y
+
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl restart catalogue
