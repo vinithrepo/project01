88b91b4a9509f80d4f0d6bab9727e35091fe9dbe
vinith <vinithamm007@gmail.com>
2024-07-01 18:17:49 +0530

    pasting conf file after installing nginx

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 
 dnf install nginx -y
-
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
