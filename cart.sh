8eda63ae97ed7fd0d8e8bad038ecc7f420b2f2e0
Vinith Amma <vinithamma007@gmail.com>
2024-07-02 19:59:08 +0530

    cart script added

---
+++ cart.sh
@@ -0,0 +1,22 @@
+cp cart.service /etc/systemd/system/cart.service
+
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+mkdir /app
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+unzip /tmp/cart.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl restart cart
