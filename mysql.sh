ddade168d482b472996b6ce1398b7ed61d24f4ae
Vinith Amma <vinithamma007@gmail.com>
2024-07-02 20:43:27 +0530

    mysql script adding

---
+++ mysql.sh
@@ -0,0 +1,9 @@
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+dnf module disable mysql -y
+
+dnf install mysql-community-server -y
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+systemctl enable mysqld
+systemctl restart mysqld
