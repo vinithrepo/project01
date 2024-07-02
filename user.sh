1d817f026b1f3b5bd4b5ae10307be2fe1a736e22
Vinith Amma <vinithamma007@gmail.com>
2024-07-02 19:19:37 +0530

    redis,user scripts added

---
+++ user.sh
@@ -0,0 +1,23 @@
+cp user.service /etc/systemd/system/user.service
+
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+
+mkdir /app
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable user
+systemctl restart user
