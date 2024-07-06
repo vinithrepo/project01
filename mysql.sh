94506a40e5d2cf426988e8fc45b9ad5e6a57780a
Vinith Amma <vinithamma007@gmail.com>
2024-07-06 19:23:27 +0530

    using spl vars for password

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,15 @@
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo Input Password Missing
+  exit 1
+fi
+
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf module disable mysql -y
 
 dnf install mysql-community-server -y
 
-mysql_secure_installation --set-root-pass RoboShop@1
-
 systemctl enable mysqld
 systemctl restart mysqld
+
+mysql_secure_installation --set-root-pass ${mysql_root_password}
