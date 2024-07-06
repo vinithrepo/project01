ed87fa79c2fa86c6fb2d6dd1b42b92f7795479c1
Vinith Amma <vinithamma007@gmail.com>
2024-07-06 10:22:00 +0530

    nfr

--- shipping.sh
+++ shipping.sh
@@ -1,24 +1,7 @@
-cp shipping.service /etc/systemd/system/shipping.service
-dnf install maven -y
+component=shipping
 
-useradd roboshop
+schema_type=mysql
 
-rm -rf /app
-mkdir /app
+source common.sh
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-dnf install mysql -y
-
-mysql -h mysql.vinithaws.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl daemon-reload
-
-
-systemctl enable shipping
-systemctl restart shipping
+func_java
