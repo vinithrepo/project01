57692945b1129f98e187441db0eae0e62243b7ef
Vinith Amma <vinithamma007@gmail.com>
2024-07-06 19:41:38 +0530

    exit status for redis

--- redis.sh
+++ redis.sh
@@ -1,22 +1,22 @@
 source common.sh
 
 echo  -e "\e[36m>>>> rpm remirepo download <<<<\e[0m"   | tee -a  ${log}
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>{log}
 func_exit_status
 
 echo  -e "\e[36m>>>> enable redis service <<<<\e[0m"   | tee -a  ${log}
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y  &>>{log}
 func_exit_status
 
 echo  -e "\e[36m>>>> install redis service <<<<\e[0m"   | tee -a  ${log}
-dnf install redis -y
+dnf install redis -y  &>>{log}
 func_exit_status
 
 echo  -e "\e[36m>>>> listen address <<<<\e[0m"   | tee -a  ${log}
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf  &>>{log}
 func_exit_status
 
 echo  -e "\e[36m>>>> start redis service <<<<\e[0m"   | tee -a  ${log}
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis  &>>{log}
+systemctl restart redis  &>>{log}
 func_exit_status
