3929ac35055229b7800096a9d63527d2bdc5b223
Vinith Amma <vinithamma007@gmail.com>
2024-07-06 19:32:44 +0530

    using spl vars for password

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,18 @@
+source common.sh
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo  -e "\e[36m>>>> copy mongo repo <<<<\e[0m"   | tee -a  ${log}
+cp mongo.repo /etc/yum.repos.d/mongo.repo   &>>${log}
+func_exit_status
 
-dnf install mongodb-org -y
+echo  -e "\e[36m>>>> install mongodb service <<<<\e[0m"   | tee -a  ${log}
+dnf install mongodb-org -y   &>>${log}
+func_exit_status
 
-sed -i 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf
+echo  -e "\e[36m>>>> update listen address <<<<\e[0m"   | tee -a  ${log}
+sed -i 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf   &>>${log}
+func_exit_status
 
-systemctl enable mongod
-systemctl restart mongod
+echo  -e "\e[36m>>>> start mongodb <<<<\e[0m"   | tee -a  ${log}
+systemctl enable mongod   &>>${log}
+systemctl restart mongod   &>>${log}
+func_exit_status
--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,18 @@
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
-  echo Input Password Missing
+  echo e "\e[34m>>>> INPUT PASSWORD MISSING <<<<\e[0m"
   exit 1
 fi
+source common.sh
 
+echo  -e "\e[36m>>>> copy mysql repo <<<<\e[0m"   | tee -a  ${log}
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo  -e "\e[36m>>>> disable mysql <<<<\e[0m"   | tee -a  ${log}
 dnf module disable mysql -y
-
+echo  -e "\e[36m>>>> install mysql  <<<<\e[0m"   | tee -a  ${log}
 dnf install mysql-community-server -y
-
+echo  -e "\e[36m>>>> start mysql service <<<<\e[0m"   | tee -a  ${log}
 systemctl enable mysqld
 systemctl restart mysqld
-
+echo  -e "\e[36m>>>> set root password  <<<<\e[0m"   | tee -a  ${log}
 mysql_secure_installation --set-root-pass ${mysql_root_password}
