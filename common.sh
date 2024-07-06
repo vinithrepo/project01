b31afef0a5dca07dc8636f132b70de161090f9c9
Vinith Amma <vinithamma007@gmail.com>
2024-07-06 18:08:54 +0530

    exit status space

--- common.sh
+++ common.sh
@@ -109,7 +109,7 @@ func_systemd(){
   func_exit_status
 }
 func_exit_status(){
-  if[ $? -eq 0]; then
+  if [ $? -eq 0 ] ; then
     echo -e "\e[32m SUCCESS \e[0m"
   else
     echo -e "\e[31m FAILURE \e[0m"
